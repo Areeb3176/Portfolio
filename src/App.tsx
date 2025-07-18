9334d88853ca9f7635d8047a860562cdc3a94083
Areeb3176 <kit7355608@gmail.com>
2025-07-18 17:48:59 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -175,6 +175,7 @@ const certifications = [
     url: 'https://github.com/Areeb3176/Logo-Generator',
     image: 'l1.JPG'
   },
+    {
     id: 5, 
     title: 'Logo Generator', 
     description: 'The Logo Generator module allows users to create high-quality, brand-ready logos by simply describing their business or brand idea.This feature combines Stable Diffusion XL (SDXL) with a specialized LoRA model and Gemini AI to enhance prompts and produce visually appealing, AI-generated logos. The system also applies post-processing to refine the output for clarity, color balance, and resolution making the logos suitable for digital or print use.Users just describe their concept and pick shape/style no design skills needed.Generates multiple logo variations at once.Ideal for startups, freelancers, and brand creators', 
